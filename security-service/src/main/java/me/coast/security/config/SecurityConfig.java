41686f01af03ace36bc63412a2ffe00b323d09cc
dengmeiliang <dengmeiliang@shoplineapp.com>
2022-08-21 23:11:33 +0800

    测试security

---
+++ security-service/src/main/java/me/coast/security/config/SecurityConfig.java
@@ -0,0 +1,31 @@
+package me.coast.security.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Bean
+    PasswordEncoder passwordEncoder() {
+        return NoOpPasswordEncoder.getInstance();
+    }
+    
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.httpBasic();
+        super.configure(http);
+    }
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication()
+                .withUser("ty1972")
+                .password("123456").roles("admin");
+    }
+    
+}
--- security-service/src/main/java/me/coast/security/controller/HelloController.java
+++ security-service/src/main/java/me/coast/security/controller/HelloController.java
@@ -1,5 +1,7 @@
 package me.coast.security.controller;
 
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,6 +10,7 @@ public class HelloController {
 
     @GetMapping("/hello")
     public String hello(){
+        PasswordEncoder passwordEncoder = NoOpPasswordEncoder.getInstance();
         return "hello";
     }
 }
