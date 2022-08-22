75cc5d3f819534a7d82e992f64ad2ccdaae4fb4f
dengmeiliang <dengmeiliang@shoplineapp.com>
2022-08-22 23:15:35 +0800

    feat: update query condition (issue #JIRA DRC-25)

--- auth-server/src/main/java/me/coast/auth/server/config/AuthServerConfig.java
+++ auth-server/src/main/java/me/coast/auth/server/config/AuthServerConfig.java
@@ -1,15 +1,12 @@
 package me.coast.auth.server.config;
 
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.Resource;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
-import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerSecurityConfigurer;
 import org.springframework.security.oauth2.provider.token.TokenStore;
