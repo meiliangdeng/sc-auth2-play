472145938f66ca5f2e5b2366d1ae2447ac33fbc6
meiliangdeng <meiliangdeng@163.com>
2022-08-24 01:35:32 +0800

    cdc测试

--- auth-server/src/main/java/me/coast/auth/server/config/flat/TyAuthServerConfig.java
+++ auth-server/src/main/java/me/coast/auth/server/config/flat/TyAuthServerConfig.java
@@ -2,13 +2,16 @@ package me.coast.auth.server.config.flat;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
+import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerSecurityConfigurer;
 
 @Configuration
 @EnableAuthorizationServer
+@Order(Integer.MIN_VALUE)
 public class TyAuthServerConfig extends AuthorizationServerConfigurerAdapter {
     
     @Bean
@@ -18,12 +21,21 @@ public class TyAuthServerConfig extends AuthorizationServerConfigurerAdapter {
  
     @Override
     public void configure(ClientDetailsServiceConfigurer clients) throws Exception{
-                clients.inMemory()
-                .withClient("client1")
-                .secret(passwordEncoder().encode("server1"))
-                .authorizedGrantTypes("password","refresh_token","authorization_code")
-                .scopes("all")
-                .redirectUris("http://127.0.0.1:8082/say");
+        clients.inMemory()
+        .withClient("client1")
+        .secret("server1")
+        .authorizedGrantTypes("refresh_token", "password", "authorization_code")
+        .scopes("all")
+        .accessTokenValiditySeconds(3000)
+        .autoApprove(true);
+    }
+
+    @Override
+    public void configure(AuthorizationServerSecurityConfigurer security) throws Exception {
+        security
+                .allowFormAuthenticationForClients()
+                .tokenKeyAccess("isAuthenticated()")
+                .checkTokenAccess("permitAll()");
     }
 
 }
--- auth-server/src/main/java/me/coast/auth/server/config/flat/TyWebSecurityConfig.java
+++ auth-server/src/main/java/me/coast/auth/server/config/flat/TyWebSecurityConfig.java
@@ -1,59 +1,33 @@
 package me.coast.auth.server.config.flat;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.security.config.annotation.web.configurers.ExpressionUrlAuthorizationConfigurer;
 
 @Configuration
 @EnableWebSecurity
-@EnableGlobalMethodSecurity(prePostEnabled = true, securedEnabled = true, jsr250Enabled = true)
-
 public class TyWebSecurityConfig extends WebSecurityConfigurerAdapter {
  
+    // @Bean
+    // @Override
+    // public AuthenticationManager authenticationManagerBean() throws Exception {
+    //     return super.authenticationManagerBean();
+    // }
+
     @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        // 禁用 csrf, 由于使用的是JWT，我们这里不需要csrf
-        http.csrf().disable();
-        http.authorizeRequests()
+    public void configure(HttpSecurity http) throws Exception {
+        ExpressionUrlAuthorizationConfigurer<HttpSecurity>.ExpressionInterceptUrlRegistry registry =
+                http.formLogin()
+                        .and()
+                        .authorizeRequests();
+        registry.antMatchers("/oauth/**").permitAll();
+        registry.antMatchers("/login/**").permitAll();
+        registry.antMatchers("/logout/**").permitAll();
+        
+        registry.anyRequest().authenticated()
                 .and()
-                .formLogin() //新增login form支持用户登录及授权
-                //自定义登录页面
-                .loginPage("http://localhost:8082/login")
-                //认证成功跳转的页面
-                .successForwardUrl("http://localhost:8082/index")
-                .failureForwardUrl("http://localhost:8082/login")
-                .and()
-                .logout()
-                .logoutUrl("/logout")
-                .logoutSuccessUrl("http://localhost:3000/login")
-                .permitAll()
-
-                //wagger 相关请求放行
-                .and()
-                .authorizeRequests()
-                //oauth相关请求放行
-                .antMatchers("/oauth/**").permitAll()
-                .antMatchers("/login/**").permitAll()
-                .antMatchers("/logout/**").permitAll()
-                .antMatchers("/swagger-ui.html").permitAll()
-                .antMatchers("/swagger-resources").permitAll()
-                .antMatchers("/swagger-ui/*").permitAll()
-                .antMatchers("/v2/api-docs").permitAll()
-                .antMatchers("/v3/api-docs").permitAll()
-                .antMatchers("/webjars/**").permitAll()
-
-                // 其他所有请求需要身份认证
-                .anyRequest().authenticated()
-
-                //配置自定义异常处理
-                .and().exceptionHandling();
+                .csrf().disable();
     }
 }
