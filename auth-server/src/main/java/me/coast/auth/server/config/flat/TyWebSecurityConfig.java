9f5ce5f40bf79d61b0df7708796fd70f4e775b73
meiliangdeng <meiliangdeng@163.com>
2022-08-31 23:55:08 +0800

    增加密码验证

--- auth-server/src/main/java/me/coast/auth/server/config/flat/TyWebSecurityConfig.java
+++ auth-server/src/main/java/me/coast/auth/server/config/flat/TyWebSecurityConfig.java
@@ -10,8 +10,6 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.annotation.web.configurers.ExpressionUrlAuthorizationConfigurer;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 
 import me.coast.auth.server.service.impl.UserDetailsServiceImpl;
 
